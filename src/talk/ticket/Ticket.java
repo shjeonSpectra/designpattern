ab6b2e307008d1ca049d4e3c04842ce679ff371b
shjeon <shjeon@spectra.co.kr>
2018-04-16 18:40:22 +0900

    ticket 생성

---
+++ src/talk/ticket/Ticket.java
@@ -0,0 +1,55 @@
+package talk.ticket;
+
+/**
+ * Created by shjeon on 2018-04-16.
+ */
+public class Ticket {
+
+    private String id;
+    private String status;
+    private String accountId;
+    private String customerId;
+    private String date;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+
+}
